a9df1b3e0922b23e00b4e71747b94ae64075bca4
Connor Whisman <connorwhisman17@gmail.com>
2023-03-15 16:01:29 -0400

    Now able to add records to container items. Notes.

--- src/app/controllers/container.ctrl.js
+++ src/app/controllers/container.ctrl.js
@@ -36,9 +36,36 @@ app.controller('containerCtrl', [
                 }
             }
             databaseSvc.saveData(data);
-            
+
             $location.url('/'+ newTotal);
         }
+
+        // STILL NEEDS TO BE HANDLED ON SERVER IN ORDER TO SAVE 
+        $scope.addRecord = function(container) {
+            var index = $scope.containers.indexOf(container);
+
+            var containerRecords = $scope.containers[index].item.records;
+
+            containerRecords.push({name: 'New Record', date: new Date(), data1: 3});
+
+
+            console.log('index: ', index);
+            console.log('records: ', containerRecords);
+        }
+
+        // -- ADD ABILITY TO CUSTOMIZE CONTAINER AND RECORD DATA THAT IS BEING ADDED. USE SOMETHING LIKE A MODAL WINDOW POPUP RATHER THAN JUST A BUTTON
+        // -- ADD ABILITY TO CHANGE ITEM THAT IS IN CONTAINER !! 
+
+        // -- IF ITEM IS REMOVED FROM CONTAINER, DATA IS SAVED. HOWEVER A NEW INSTANCE WILL BE CREATED WHEN SAME TYPE OF ITEM IS ADDED BACK
+        // -- CREATE CLASSES FOR EACH TYPE OF OBJECT?
+        // -- ADD LIST OF ITEMS TO CHOOSE FROM THAT CAN BE STORED IN CONTAINERS
+
+        // -- SOME SORT OF 'VAULT' TO VIEW PAST DATA THAT NO LONGER LIVES IN THE CONTAINER
+
+
+        // OTHER SMALL FEATURES:
+        // -- WAY TO RENAME CONTAINERS
+
 
         
         $log.info('Database Response: ', $scope.database);
