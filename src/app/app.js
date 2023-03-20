8d4b16a072a1b9ffc9ac453fd47c49180cf1375b
Connor Whisman <connorwhisman17@gmail.com>
2023-03-20 14:21:44 -0400

    Start building item choices/options

--- src/app/app.js
+++ src/app/app.js
@@ -16,5 +16,10 @@ app.config(function($routeProvider) {
 app.controller('appCtrl', [
     '$rootScope',
     function($rootScope) {
-        $rootScope.containers = [];
+        $rootScope.containers   = [];
+        $rootScope.itemOpts     = [
+            {name: 'Choice 1', dateCreated: new Date(), description: 'test 1', records: []},
+            {name: 'Choice 2', dateCreated: new Date(), description: 'test 2', records: []},
+            {name: 'Choice 3', dateCreated: new Date(), description: 'test 3', records: []},
+        ];
 }]);
