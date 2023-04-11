1ff05c86d11e6afdeeb7af1ec25cc6e3270a8e2d
Connor Whisman <connorwhisman17@gmail.com>
2023-04-11 13:43:11 -0400

    Put archiveCtrl back into its own file

---
+++ src/app/archive/archive.ctrl.js
@@ -0,0 +1,5 @@
+archive.controller('archiveCtrl', [
+    '$scope', 
+    function($scope) {
+        $scope.testStr = 'Works';
+}]);
--- src/app/archive/archive.js
+++ src/app/archive/archive.js
@@ -1,21 +1,10 @@
-angular.module('archive', [
+var archive = angular.module('archive', [
     'ngResource'
 ]);
 
 
-angular.module('archive')
-    .directive('archiveDiv', function() {
+archive.directive('archiveDiv', function() {
         return {
             templateUrl: 'archive/templates/archive.html'
         }
     });
-
-
-angular.module('archive')
-    .controller('archiveCtrl', [
-        '$scope', 
-        function($scope) {
-            $scope.testStr = 'Works';
-    }]);
-
-    
